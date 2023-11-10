1bc2ebdff4864bb598ba13508b06b84d359f9336
Ionut Panduru <panduru.ionut@hotmail.com>
2023-11-10 15:20:33 +0200

    feat: dockerize redis, echo and queue worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ cp .env.example .env
 
 php artisan key:generate
 
+export $(grep -v '^#' /var/www/html/.env | xargs -0)
+
+if [[ -z $(mysql -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" -e "SHOW DATABASES LIKE '$DB_DATABASE'";) ]]; then
+    mysql -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" -e "CREATE DATABASE $DB_DATABASE;"
+fi
+
 php artisan migrate
 
 php artisan serve --host=0.0.0.0
